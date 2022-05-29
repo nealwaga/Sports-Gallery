e453ce7100704c1c9cc26f18568713eb2b88f658
nealwaga <waganeal@gmail.com>
2022-05-29 08:17:38 +0300

    updated settings file

--- images/views.py
+++ images/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"all-images/image.html", {"image":image})
