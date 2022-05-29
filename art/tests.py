b9b6a77bde672342bb2145f9fc64e3d424333751
nealwaga <waganeal@gmail.com>
2022-05-29 21:17:36 +0300

    running tests

--- art/tests.py
+++ art/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import *
+from datetime import dt
+
 
 # Create your tests here.
+class PaparazzoTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.neal= Paparazzo(first_name = 'Neal', last_name ='Waga', email ='nw@gmail.com')
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neal,Paparazzo))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.neal.save_paparazzo()
+        paparazzo = Paparazzo.objects.all()
+        self.assertTrue(len(paparazzo) > 0)
+
