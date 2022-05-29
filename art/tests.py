d5a1d0ad06df42e3f4b0152d986c1d96077c6df2
nealwaga <waganeal@gmail.com>
2022-05-29 21:17:53 +0300

    running tests

--- art/tests.py
+++ art/tests.py
@@ -19,3 +19,35 @@ class PaparazzoTestClass(TestCase):
         paparazzo = Paparazzo.objects.all()
         self.assertTrue(len(paparazzo) > 0)
 
+class PicTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new paparazzo and saving it
+        self.neal= Paparazzo(first_name = 'Neal', last_name ='Waga', email ='wn@gmail.com')
+        self.neal.save_paparazzo()
+
+        # Creating a new tag and saving it
+        self.new_tags = tags(name = 'testing')
+        self.new_tags.save()
+
+        self.new_photo= Pic(photo = 'pictures/6_1_of_1.jpg',caption = 'Test caption',paparazzo = self.neal)
+        self.new_photo.save()
+
+        self.new_photo.tag.add(self.new_tags)
+
+    def test_get_news_today(self):
+        today_art = Pic.todays_art()
+        self.assertTrue(len(today_art)>0)
+
+  
+
+    def test_get_art_by_date(self):
+        test_date = '2020-06-24'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        art_by_date = Pic.days_news(date)
+        self.assertTrue(len(art_by_date) > 0)
+
+    def tearDown(self):
+        Paparazzo.objects.all().delete()
+        tags.objects.all().delete()
+        Pic.objects.all().delete()
