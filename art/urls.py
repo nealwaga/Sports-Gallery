dd943917e6f8c8252b60a20318d6aba790214dca
nealwaga <waganeal@gmail.com>
2022-05-29 09:46:39 +0300

    updated settings file

--- art/urls.py
+++ art/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns=[
     url(r'^$',views.art_of_day,name='artToday'),
     url(r'^search/', views.search_results, name='search_results'), 
--- gallery/settings.py
+++ gallery/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', 
             ],
         },
     },
@@ -78,8 +79,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': 'neal',
+        'PASSWORD': 'Wneal9.'
     }
 }
 
