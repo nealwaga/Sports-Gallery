d0fb538a158781182b44a321f3134f7f6bf42b72
nealwaga <waganeal@gmail.com>
2022-05-29 18:05:10 +0300

    adding models to admin

--- art/admin.py
+++ art/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class PicAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+admin.site.register(Pic, PicAdmin)
+admin.site.register(Paparazzo)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(tags)
