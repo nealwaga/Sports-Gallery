a7f52a83595789647d0f35c7f272904b68b303a0
nealwaga <waganeal@gmail.com>
2022-05-29 16:00:05 +0300

    added template file

--- gallery/settings.py
+++ gallery/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'art',
     'bootstrap3',
     'django.contrib.staticfiles',
+    'art.apps.ArtConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
